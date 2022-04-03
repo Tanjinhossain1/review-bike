df90f8bf59f2571ff6bde789867a6ec4bd2ebf5b
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-03 22:45:35 +0600

    home text,button,img done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='text-center p-6 text-2xl md:flex justify-center'>
+            <div className='mr-12'>
+                <NavLink className={({ isActive }) => isActive ? ' border-b-4 border-orange-600 font-semibold text-orange-700' : 'font-semibold border-b-4 '} to='/'>Home</NavLink>
+            </div>
+            <div className=''>
+                <NavLink className={({ isActive }) => isActive ? ' border-b-4 font-semibold border-orange-600   text-orange-700' : 'font-semibold border-b-4 '} to='reviews'>REVIEWS</NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h1>hi i am review</h1>
+        </div>
+    );
+};
+
+export default Reviews;
