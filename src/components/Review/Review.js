d7aae30ecf6fd7b00c7be13c1b9c72aa8d88aa98
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-04 00:20:01 +0600

    set all reviews

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Review = ({ review }) => {
     console.log(review)
-    const { name, userImg, rating } = review;
+    const { name, userImg, rating, userReview } = review;
     return (
         <div className='border-2 p-8 m-12 rounded-lg'>
-            <img src={userImg} alt="" />
-            <h1 className='mt-4 text-2xl'>Name: {name}</h1>
-            <p>Rating: <span className='text-pink-500 font-bold'>{rating}</span></p>
+            <img className='rounded-full' src={userImg} alt="" />
+            <h1 className='mt-4 text-3xl'>Name: {name}</h1>
+            <p className='text-xl'>Rating: <span className='text-pink-500 font-bold'>{rating}</span></p>
+            <p className='text-xl'>Review: {userReview}</p>
         </div>
     );
 };
