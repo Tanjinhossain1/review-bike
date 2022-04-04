d2df8b6c5d0137fab7268905880604c04e178241
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-04 16:10:16 +0600

    add spiner

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import useReviews from '../../hooks/useReviews';
 import Review from '../Review/Review';
-
+import './Reviews.css'
 const Reviews = () => {
     const [reviews, setReviews] = useReviews();
+    const Spinner = () => <div className="loader"></div>;
 
     return (
-        <div>
-           <div className='grid md:grid-cols-2 lg:grid-cols-3 mt-12'>
+        <div className='mt-20 '>
+            <div className='flex justify-center  text-center'>
+            {reviews.length <= 0 && <Spinner/>}
+            </div>
+           <div className='grid md:grid-cols-2 lg:grid-cols-3 ' >
                {
                    reviews.map(review => <Review key={review.id} review={review}></Review>)
                }
